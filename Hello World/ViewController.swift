e480d7b01f39bf0ecbc292d2f1bf1959cb0145f2
Felipe Christian Lautenschlager <felipelauten@gmail.com>
2019-01-16 19:31:01 +0100

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Dev Lauten on 16/01/19.
+//  Copyright © 2019 DevLauten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
