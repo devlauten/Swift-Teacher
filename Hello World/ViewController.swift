0fe5cd45921dbce8988389865e17d493873cec94
Felipe Christian Lautenschlager <felipelauten@gmail.com>
2019-01-16 20:31:11 +0100

    Initial application code

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // - MARK: Outlets
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    // - MARK: ViewController lifecycle method
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // - MARK: Action
+    @IBAction func sayHello(_ sender: Any) {
+        let nameString = self.name.text
+        
+        // Optional String
+        if let name = nameString {
+            print("User \(name) says hello!")
+            
+            // Print the value to the user
+            self.label.text = "Hello, \(name)!"
+        } else {
+            self.label.text = "Hello World !"
+        }
+        
+        // That's all! You did it!
+    }
+    
 }
 
